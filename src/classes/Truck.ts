5dd42fdc267df777ebc1d71fa8136a8d5e04e8c5
reyesrj2012 <reyesrj2012@gmail.com>
2024-08-28 18:54:16 -0500

    changes

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -61,9 +61,22 @@ class Truck extends Vehicle {
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
     // TODO: Get the make an model of the vehicle if it exists
+    vehicle.make 
+    if (vehicle.make){
+
+    }
+    
     // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
-    // TODO: If it is, log that the vehicle is being towed
+    vehicle.weight 
+    if (vehicle.weight<=this.towingCapacity){
+      // TODO: If it is, log that the vehicle is being towed
+        console.log ("Vehicle being towed")
+    }
     // TODO: If it is not, log that the vehicle is too heavy to be towed
+      else { 
+          console.log ("Vehicle cannot be towed")
+      }
+
   }
 
   // TODO: Override the printDetails method from the Vehicle class
